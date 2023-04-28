7947c9e74dd1cdf5d242f2be2460a4ce0231042d
mari <_mari@tuta.io>
2023-04-28 20:05:47 +0800

    handle printf arguments properly in tier0 log

--- crates/bevy/src/tier0.rs
+++ crates/bevy/src/tier0.rs
@@ -13,28 +13,19 @@ pub unsafe fn log(
     severity: sys::LoggingSeverity_t,
     mut args: ffi::VaListImpl<'_>,
 ) -> sys::LoggingResponse_t {
-    assert_non_null!(format);
+    let mut message = String::new();
 
-    let format = CStr::from_ptr(format);
-    let args = if format.to_string_lossy().trim() == "%s" {
-        let arg: *const ffi::c_char = args.arg();
-
-        assert_non_null!(arg);
-
-        let arg = CStr::from_ptr(arg);
-
-        arg.to_string_lossy()
-    } else {
-        format.to_string_lossy()
-    };
-
-    let args = args.trim();
+    printf_compat::format(
+        format,
+        args.as_va_list(),
+        printf_compat::output::fmt_write(&mut message),
+    );
 
     match severity {
-        sys::LoggingSeverity_t_LS_ERROR => error!("{args}"),
-        sys::LoggingSeverity_t_LS_WARNING => warn!("{args}"),
-        sys::LoggingSeverity_t_LS_MESSAGE => info!("{args}"),
-        _ => trace!("{args}"),
+        sys::LoggingSeverity_t_LS_ERROR => error!("{message}"),
+        sys::LoggingSeverity_t_LS_WARNING => warn!("{message}"),
+        sys::LoggingSeverity_t_LS_MESSAGE => info!("{message}"),
+        _ => trace!("{message}"),
     }
 
     sys::LoggingResponse_t_LR_CONTINUE
